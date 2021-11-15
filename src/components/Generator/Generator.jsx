3e4e638a747b587708a416fb1b0ad26df1a29025
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-11-15 16:39:45 +0100

    useState for the first time

--- src/components/Generator/Generator.jsx
+++ src/components/Generator/Generator.jsx
@@ -1,18 +1,24 @@
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-// import { Fragment } from 'react';
 
 import { Text } from '../Text';
 import { Button } from '../Button';
 
 function Generator() {
+  // let uuid = uuidv4();
+  const componentState = useState(uuidv4()); // -> [0, 1]
+  const componentStateValue = componentState[0];
+  const setComponentStateValue = componentState[1];
 
   const handleClick = (event) => {
-    console.log('Event: ', event);
+    // console.log('Event: ', event);
+    // uuid = uuidv4();
+    setComponentStateValue(uuidv4());
   }
 
   return (
     <>
-      <Text>Your UUID is: {uuidv4()}</Text>
+      <Text>Your UUID is: {componentStateValue}</Text>
       <Button label="Refresh" onClick={handleClick} />
     </>
   );
