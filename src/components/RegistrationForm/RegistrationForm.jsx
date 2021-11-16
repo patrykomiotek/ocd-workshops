2ce4af05243a9c95ac8a826c3ca98f7e60c23212
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-11-16 12:20:47 +0100

    generic state change

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -8,25 +8,34 @@ function RegistrationForm() {
   });
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.dir(event.target[0].value);
-    console.dir(event.target["email"].value); // document.getElementById("email");
+    // console.dir(event.target[0].value);
+    // console.dir(event.target["email"].value); // document.getElementById("email");
     // console.log(event.target);
   }
   const handleChange = (event) => {
     //console.log('Value is: ', event.target.value);
     // {...state } -> { email: value, password: value }
+    // event.target.name
+    // setState({
+    //   ...state,
+    //   email: event.target.value,
+    // });
+    const name = event.target.name; // name="email", name="password"
+    const value = event.target.value;
+    console.log("name: ", name);
     setState({
       ...state,
-      email: event.target.value,
+      [name]: value // -> email, password, emailTest
     });
   }
 
   // <a href="http://eveil.com" onClick={(event) => event.preventDefault()}>Click me</a>
+  const { email } = state;
 
   return (
     <form onSubmit={handleSubmit}>
       <Text>My awesome form</Text>
-      <Text>Your email is: {state.email}</Text>
+      <Text>Your email is: {email}</Text>
       <div>
         <label htmlFor="email">E-mail: </label>
         <input id="email" name="email" type="text" onChange={handleChange} />
@@ -34,7 +43,7 @@ function RegistrationForm() {
       </div>
       <div>
         <label htmlFor="password">Password: </label>
-        <input id="password" name="password" type="password" />
+        <input id="password" name="password" type="password" onChange={handleChange} />
       </div>
       <div>
         <input type="submit" />
